dac43d56d742782b5728f6652ccab257c1453415
sachinkum0009 <sachinkum123567@gmail.com>
2019-11-26 10:16:49 +0530

    added the mqtt files and temperature sensor

---
+++ nodemcu_files/final_mqtt_client_temperature_humidity/final_mqtt_client_temperature_humidity.ino
@@ -0,0 +1,121 @@
+/*
+  @created by Sachin Kumar
+  @mail sachinkum123567@gmail.com
+  NodeMCU code for MQTT client broker
+  That sends the data to cloud and further an flutter application can be used to view the data
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// Soil moisture inputs
+int sensorPin = A0; 
+int sensorValue;  
+int limit = 300;
+
+const char* ssid = "Redmi 6";
+const char* password =  "123456789";
+const char* mqttServer = "soldier.cloudmqtt.com";
+const int mqttPort = 10755;
+const char* mqttUser = "aypgvnsq";
+const char* mqttPassword = "vsbiDXU4OIlB";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("esp/test", "Hello from ESP8266");
+  // client.subscribe("esp/test");
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void loop() {
+  client.loop();
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Celsius temperature: ");
+  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  delay(500);
+  
+  sensorValue = analogRead(sensorPin); 
+
+// We define the sensorValue as being the value read by the Arduino.
+
+Serial.println("Analog Value : ");
+Serial.println(sensorValue);
+
+// We display the sensorValue on the serial monitor.
+
+if (sensorValue<limit) {
+digitalWrite(13, HIGH); 
+}
+else {
+digitalWrite(13, LOW);
+ }
+delay(1000); 
+  int temp = sensors.getTempCByIndex(0);
+  int humid = sensorValue;
+  client.publish("temp", temp);
+  client.publish("humid", humid);
+  delay(500);
+}
